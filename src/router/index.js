9d188d008d3057f52c5895aaaceea07e75cd34df
SQT <2034718754@qq.com>
2025-02-12 22:53:42 +0800

    二级路由设置

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,38 @@ import Pay from '@/views/pay/index.vue'
 import Prodetail from '@/views/prodetail/index.vue'
 import Search from '@/views/search/index.vue'
 import SearchList from '@/views/search/list.vue'
+import LayoutCart from '@/views/layout/cart.vue'
+import LayoutCategory from '@/views/layout/category.vue'
+import LayoutHome from '@/views/layout/home.vue'
+import LayoutUser from '@/views/layout/user.vue'
 
 Vue.use(VueRouter)
 const router = new VueRouter({
   routes: [
     { path: '/', component: App },
     { path: '/login', component: Login },
-    { path: '/layout', component: Layout },
+    {
+      path: '/layout',
+      component: Layout,
+      children: [
+        {
+          path: '/cart',
+          component: LayoutCart
+        },
+        {
+          path: '/category',
+          component: LayoutCategory
+        },
+        {
+          path: '/home',
+          component: LayoutHome
+        },
+        {
+          path: '/user',
+          component: LayoutUser
+        }
+      ]
+    },
     { path: '/myorder', component: Myorder },
     { path: '/pay', component: Pay },
     // 为了确认商品是谁，这里使用动态路由传参,在路由中携带id
