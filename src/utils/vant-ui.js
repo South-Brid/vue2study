c431d04e1efa854e0e40f8449d49b7186d838fb2
SQT <2034718754@qq.com>
2025-02-19 19:55:12 +0800

    完成静态页面的登录布局

--- src/utils/vant-ui.js
+++ src/utils/vant-ui.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import { Button, Checkbox, CheckboxGroup, Tabbar, TabbarItem } from 'vant'
+import { Button, Checkbox, CheckboxGroup, Tabbar, TabbarItem, NavBar } from 'vant'
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Button)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
+Vue.use(NavBar)
